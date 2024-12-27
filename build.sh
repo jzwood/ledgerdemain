8fca6b973df94fe62fc7e692293755cf83cecd58
Jake Wood <jzwood14@yahoo.com>
2024-12-26 22:08:56 -0500

    switching from hashed content to versioned

--- build.sh
+++ build.sh
@@ -2,21 +2,18 @@
 
 checksum() {
   FPATH="$1"
-  PARENT="$2"
-  EXT="$3"
+  EXT="${FPATH##*.}"
   DIR=$(dirname "$FPATH")
   FILE=$(basename "$FPATH")
   HASH=$(md5sum "$FPATH" | cut -d " " -f 1)
 
   NEW_FILE="$(echo "$FILE" | cut -d "." -f 1).$HASH.$EXT"
   mv "$FPATH" "$DIR/$NEW_FILE"
-  sed -i "" "s#$FILE#$NEW_FILE#g" "$PARENT"
+  echo "s#$FILE#$NEW_FILE#g"
 }
 
 find css -type f -name "_*.css" | while read -r FPATH; do
-  checksum "$FPATH" "css/main.css" "css";
-done
-
-find css -type f -name "_*.css" | while read -r FPATH; do
-  checksum "$FPATH" "css/tutorial.css" "css";
+  SED=$(checksum "$FPATH")
+  sed -i "" "$SED" "css/main.css"
+  sed -i "" "$SED" "css/tutorial.css"
 done
