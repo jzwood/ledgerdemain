7f63b089a867331235f8d883a3eebc536f14744a
Jake Wood <jzwood14@yahoo.com>
2024-12-27 01:13:22 -0500

    fixes bump bug

--- bump.sh
+++ bump.sh
@@ -2,8 +2,9 @@
 
 VERSION=1
 
-PREV_VERSION="v$((VERSION - 1))"
-mv "css/$PREV_VERSION/" "css/v$VERSION/" 2>/dev/null
-mv "js/$PREV_VERSION/" "js/v$VERSION/" 2>/dev/null
+PREV_VERSION=$((VERSION - 1))
+mv "css/v$PREV_VERSION/" "css/v$VERSION/" 2>/dev/null
+mv "js/v$PREV_VERSION/" "js/v$VERSION/" 2>/dev/null
 
-sed -i "" "s/$PREV_VERSION/$VERSION/g" index.html
+sed -i "" "s/v$PREV_VERSION/v$VERSION/g" index.html
+sed -i "" "s/v$PREV_VERSION/v$VERSION/g" tutorial.html
